1bbc49f5fb5a9060c4e4fc229fa15e01b3cf17c5
bayazidhassan <bayazidhassan776@gmail.com>
2026-04-11 23:41:28 +0600

    proxy

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,17 @@
+import { Suspense } from 'react';
 import LoginForm from '../../../components/auth/LoginForm';
 
 const LoginPage = () => {
-  return <LoginForm></LoginForm>;
+  return (
+    /*
+    Next.js App Router requires Suspense boundary for client hooks like useSearchParams() to prevent prerendering failure during static generation.
+
+    here, LoginFrom contains useSearchParams()
+    */
+    <Suspense fallback={null}>
+      <LoginForm></LoginForm>
+    </Suspense>
+  );
 };
 
 export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import CTABanner from '../components/home/CTABanner';
 import FAQ from '../components/home/FAQ';
 import Features from '../components/home/Features';
-import Footer from '../components/home/Footer';
 import Hero from '../components/home/Hero';
 import HowItWorks from '../components/home/HowItWorks';
 import MentorShowcase from '../components/home/MentorShowcase';
 import Testimonials from '../components/home/Testimonials';
+import Footer from '../components/shared/Footer';
 import Navbar from '../components/shared/Navbar';
 
 const HomePage = () => {
