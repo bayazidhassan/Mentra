093b9db941fb2ad3201a2141cc8626401cae3258
bayazidhassan <bayazidhassan776@gmail.com>
2026-05-04 00:18:14 +0600

    notification dropdown

--- components/modal/NotificationBell.tsx
+++ components/modal/NotificationBell.tsx
@@ -132,7 +132,7 @@ const NotificationBell = () => {
 
       {/* Dropdown */}
       {open && (
-        <div className="absolute right-0 top-11 w-[calc(100vw-1rem)] md:w-80 bg-white border border-gray-200 rounded-2xl shadow-lg z-50 overflow-hidden">
+        <div className="fixed top-16 left-1/2 -translate-x-1/2 w-[min(360px,calc(100vw-80px))] sm:absolute sm:top-14 sm:left-auto sm:translate-x-0 sm:right-0 sm:w-80 bg-white border border-gray-200 rounded-2xl shadow-lg z-5 overflow-hidden">
           {/* Header */}
           <div className="flex items-center justify-between px-4 py-3 border-b border-gray-100">
             <h3
