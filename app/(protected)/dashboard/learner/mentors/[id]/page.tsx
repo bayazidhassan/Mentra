26937c634f5beffee67f5a4ae06fc9bb4a1fefee
bayazidhassan <bayazidhassan776@gmail.com>
2026-05-14 08:40:21 +0600

    await params

--- app/(protected)/dashboard/learner/mentors/[id]/page.tsx
+++ app/(protected)/dashboard/learner/mentors/[id]/page.tsx
@@ -1,14 +1,15 @@
 import { cookies } from 'next/headers';
 import MentorProfileClientPage from '../../../../../../components/dashboard/learner/MentorProfileClientPage';
-import MentorProfilePage from './MentorProfilePage';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const MentorProfilePage = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
+
   const cookieStore = await cookies();
   const token = cookieStore.get('accessToken')?.value;
 
-  const res = await fetch(`${BASE_URL}/api/v1/mentor/${params.id}`, {
+  const res = await fetch(`${BASE_URL}/api/v1/mentor/${id}`, {
     headers: {
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
